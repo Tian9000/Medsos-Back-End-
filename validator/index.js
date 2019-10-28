78ba626304898a66d27bb5b8d58d855f75a3f959
Tian9000 <wijayacristian220@gmail.com>
2019-10-28 11:38:53 +0700

    Finish The Back-End For Media Sosial Web

---
+++ validator/index.js
@@ -0,0 +1,51 @@
+exports.createPostValidator = (req, res, next) => {
+    //title
+    req.check('title', 'Write a title').notEmpty();
+    req.check('title', 'Title must be between 4 to 150 characters').isLength({
+        min: 4,
+        max: 150
+    });
+    //body
+    req.check('body', 'Write a body').notEmpty();
+    req.check('body', 'Body must be between 4 to 200 characters').isLength({
+        min: 4,
+        max: 200
+    });
+    //check for errors
+    const errors = req.validationErrors()
+    //if error show the first one as they happen
+    if(errors){
+        const firstError = errors.map((error) => error.msg)[0]
+        return res.status(400).json({error:firstError})
+    }
+    //proceed to next middleware
+    next();
+};
+
+exports.userSignupValidator = (req, res, next) => {
+    // name is not null and between 4-10 characters
+    req.check('name', "Name is required").notEmpty()
+    req.check('email', 'Email must be between 3 to 32 characters')
+    .matches(/.+\..+/)
+    .withMessage('Email must contain @')
+    .isLength({
+        min:4,
+        max: 2000
+    })
+    // check for password
+    req.check('password', 'Password is required').notEmpty();
+    req.check('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must contain at least 6 characters')
+    .matches(/\d/)
+    .withMessage('Password must contain a number')
+    // chek for errors
+    const errors = req.validationErrors()
+    //if error show the first one as they happen
+    if(errors){
+        const firstError = errors.map((error) => error.msg)[0]
+        return res.status(400).json({error:firstError})
+    }
+    //proceed to next middleware
+    next();
+}
